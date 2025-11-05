c7ccf2add6a94cd49f16c1e924162a8dafcd79d4
Trần Như  Quỳnh <trankienquynh.070820@gmail.com>
2025-11-05 13:20:48 +0700

    first commit vite react project

---
+++ demo01/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="container">
+        <header>
+          <h1>Hello world!</h1>
+          <h1>Trần Như Quỳnh - 22110218</h1>
+        </header>
+      </div>
+    </>
+  )
+}
+
+export default App
